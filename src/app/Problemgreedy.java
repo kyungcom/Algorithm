c4d5f6badf0026e01091383315cd2061ad9814a3
박인호 <qrdlsgh1234@khu.ac.kr>
2021-06-01 22:19:13 +0900

    reverseCard

--- src/app/Problemgreedy.java
+++ src/app/Problemgreedy.java
@@ -1,5 +1,6 @@
 package app;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.Scanner;
@@ -29,8 +30,104 @@ public class Problemgreedy {
         }
         System.out.println(group);
     }
+    
+    //이것이 코딩테스트다 p.312 곱하기 혹은 더하기
+    public static void multOrPlus(){
+        Scanner sc = new Scanner(System.in);
+        String num = sc.nextLine();
+
+        int start = 0;
+        for (int i = 0; i < num.length(); i++){
+            int next = Integer.parseInt(num.substring(i, i+1));
+            if(start * next > start + next){
+                start = start * next;
+            }else {
+                start = start + next;
+            }
+        }
+
+        System.out.println(start);
+    }
+
+    //이것이 코딩테스트다 p.313 문자열 뒤집기
+    public static int reverseString(){
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+
+        int numOfOne = 0;
+        int numOfZero = 0;
+
+        ArrayList<Integer> continuousOne = new ArrayList<Integer>();
+        ArrayList<Integer> continuousZero = new ArrayList<Integer>();
+
+        String previous = "-";
+        int continuous = 0;
+
+        for (int i = 0; i < str.length(); i++){
+            if(str.substring(i, i+1).equals("1")){
+                numOfOne++;
+                if(previous.equals("-") || previous.equals("1"))
+                    continuous++;
+                else{
+                    continuousZero.add(continuous);
+                    continuous = 1;
+                }
+                previous = "1";
+                if (i == str.length()-1){
+                    continuousOne.add(continuous);
+                }
+            } else {
+                numOfZero++;
+                if(previous.equals("-") || previous.equals("0"))
+                    continuous++;
+                else{
+                    continuousOne.add(continuous);
+                    continuous = 1;
+                }
+                previous = "0";
+                if (i == str.length()-1){
+                    continuousZero.add(continuous);
+                }
+            }
+
+        }
+
+        Collections.sort(continuousOne, Collections.reverseOrder());
+        Collections.sort(continuousZero, Collections.reverseOrder());
+
+
+        int result = 0;
+
+        if (numOfOne > numOfZero){
+            int subtract = numOfOne - numOfZero;
+            for (int i = 0; i < continuousOne.size(); i++){
+                subtract -= continuousOne.get(i);
+                result++;
+                if(subtract <= 0){
+                    return result;
+                }
+            }
+            return -1;
+        } else if (numOfOne < numOfZero){
+            int subtract = -numOfOne + numOfZero;
+            for (int i = 0; i < continuousZero.size(); i++){
+                subtract -= continuousZero.get(i);
+                result++;
+                if(subtract <= 0){
+                    return result;
+                }
+            }
+            return -1;
+
+        }
+        return 0;
+
+
+
+
+    }
 
     public static void main(String args[]){
-        guild();
+        System.out.println(reverseString());
     }
 }
