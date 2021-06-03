2aa5f58f66c34ccb72e63db831c49efa932c9161
박인호 <qrdlsgh1234@khu.ac.kr>
2021-06-03 17:36:43 +0900

    june03

--- src/app/RealizationProblem.java
+++ src/app/RealizationProblem.java
@@ -1,5 +1,8 @@
 package app;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.Scanner;
 
 public class RealizationProblem {
@@ -27,7 +30,33 @@ public class RealizationProblem {
         }
 
     }
+
+    //이것이 코딩 테스트다 p.322 문자열 재정렬
+    public static void reSort(){
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+        ArrayList<Character> chars = new ArrayList<Character>();
+        int ints = 0;
+        for (int i = 0; i < str.length(); i++){
+            char item = str.charAt(i);
+            if ((int)item >= 65){
+                chars.add(item);
+            }
+            else {
+                ints += Character.getNumericValue(item);
+            }
+        }
+
+        Collections.sort(chars);
+
+        for (int i = 0; i < chars.size(); i++){
+            System.out.print(chars.get(i));
+        }
+
+        System.out.print(ints);
+
+    }
     public static void main(String args[]){
-        lucky();
+        reSort();
     }
 }
